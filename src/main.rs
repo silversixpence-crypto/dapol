0775c56c5b970ac8f5f47cc1ce990adf825d3086
Stent <stentonian@protonmail.com>
2023-09-07 11:31:51 +0100

    Add timings to main.rs

--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,30 @@ use smtree::{
     traits::{ProofExtractable, Rand, Serializable, TypeName},
 };
 use std::time::Duration;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() {
+    let start = SystemTime::now();
+    println!("start {:?}", start);
     old();
+    let end = SystemTime::now();
+    let dur = end.duration_since(start);
+    println!("end {:?}", end);
+    println!("duration {:?}", dur);
 }
 
 fn new() {
     println!("new");
 
     let tree_height = 32;
-    let num_leaves: usize = 2usize.pow(27); // 134M
+    // let num_leaves: usize = 2usize.pow(27); // 134M
+    let num_leaves: usize = 2usize.pow(23); // 8.4M
 
     let items = build_item_list(num_leaves, tree_height);
+
+    let start = SystemTime::now();
+    println!("converting list now {:?}", start);
+
     let users: Vec<User> = items
         .iter()
         .map(|item| User {
@@ -33,6 +45,10 @@ fn new() {
             liability: item.1.get_value(),
         })
         .collect();
+
+    let end = SystemTime::now();
+    let dur = end.duration_since(start);
+    println!("done converting item list, time now {:?}, duration {:?}", end, dur);
 
     let master_secret: D256 = D256::from(3u64);
     let salt_b: D256 = D256::from(5u64);
@@ -46,7 +62,8 @@ fn new() {
 
 fn old() {
     println!("old");
-    let num_leaves: usize = 2usize.pow(27); // 134M
+    // let num_leaves: usize = 2usize.pow(27); // 134M
+    let num_leaves: usize = 2usize.pow(23); // 8.4M
 
     // bench tree height = 32
     let tree_height = 32;
@@ -71,7 +88,10 @@ fn build_item_list(
     num_leaves: usize,
     tree_height: usize,
 ) -> Vec<(TreeIndex, DapolNode<blake3::Hasher>)> {
-    let mut result = Vec::new();
+    let start = SystemTime::now();
+    println!("build_item_list {:?}", start);
+
+    let mut result = Vec::with_capacity(num_leaves);
     let mut value = DapolNode::<blake3::Hasher>::default();
     let stride = 2usize.pow(tree_height as u32) / num_leaves;
     for i in 0..num_leaves {
@@ -80,6 +100,15 @@ fn build_item_list(
         result.push((idx, value.clone()));
     }
 
+    let after_loop = SystemTime::now();
+    let dur = after_loop.duration_since(start);
+    println!("built item list (next is sorting), time now {:?}, duration {:?}", after_loop, dur);
+
     result.sort_by_key(|(index, _)| *index);
+
+    let end = SystemTime::now();
+    let dur = end.duration_since(start);
+    println!("done building item list, time now {:?}, duration {:?}", end, dur);
+
     result
 }
